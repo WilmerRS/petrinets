85b332e4d9a313f32cb1a0b1e634a0c251a51a9a
WilmerRS <est.wilmerrodriguez@gmail.com>
2023-04-10 09:31:51 -0500

    root: project created

---
+++ src/main/kotlin/com/petrinets/engine/Application.kt
@@ -0,0 +1,22 @@
+package com.petrinets.engine
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+import com.petrinets.engine.plugins.configureRouting
+import com.petrinets.engine.plugins.configureSerialization
+
+fun main() {
+    embeddedServer(
+        Netty,
+        port = 8080,
+        host = "0.0.0.0",
+        module = Application::module
+    ).start(wait = true)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
---
+++ src/test/kotlin/com/petrinets/engine/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package com.petrinets.engine
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+import io.ktor.http.*
+import com.petrinets.engine.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
